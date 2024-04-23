fb83590ed59cd709dbcee6908f3935022b2c8e62
Arunima Deb <arunima.s.deb@gmail.com>
2024-04-23 12:02:37 +0530

    Class X

---
+++ src/com/arunima/stdx/ch4/Program_E_9.java
@@ -0,0 +1,41 @@
+package com.arunima.stdx.ch4 ;
+
+import java.util.* ;
+
+/**
+ * Write a method fact(int n) to find the factorial of a number n. 
+ * 
+ * Include a main class to find the value of S where: 
+ * 
+ * n! = m! / (n—m)! 
+ * 
+ * where, 
+ * 
+ * n! = 1 x 2 x 3 x .. x n 
+ */
+public class Program_E_9
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner( System.in ) ;
+        System.out.println( "Enter two numbers : " ) ;
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        double S = fact(n)/((fact(m)*fact((n-m)))) ;
+        System.out.println(S) ;
+        
+        sc.close() ;
+    }
+    
+    public static int fact( int n ) 
+    {
+    	int f=1 ;
+    	for( int i=1; i<=n; i++ )
+    	{
+    		f=f*i ;
+    	}
+    	return f ;
+    }
+    
+}
